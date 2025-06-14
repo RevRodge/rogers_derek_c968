e88e1b6faefd9aaa25daea8a1dbb6ea3d9ebaa11
revrodge <derekrogers007@gmail.com>
2025-06-13 20:09:57 -0600

    added forms and model classes

---
+++ rogers_derek_c968/Models/Part.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rogers_derek_c968.Models
+{
+    internal class Part
+    {
+    }
+}
