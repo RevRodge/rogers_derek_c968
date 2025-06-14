10be5e52fa3efea4aa5579f330ae8ce21faccba8
revrodge <derekrogers007@gmail.com>
2025-06-13 21:26:08 -0600

    simplified my approach and named attributes to align with UML

--- rogers_derek_c968/Models/InHouse.cs
+++ rogers_derek_c968/Models/InHouse.cs
@@ -9,28 +9,6 @@ namespace rogers_derek_c968.Models
 {
     public class InHouse : Part
     {
-        private int machID;
         public int MachineID { get; set; }
-        public InHouse() { }
-        public InHouse(int partID, string name, int inventory, decimal price, int max, int min)
-        {
-            PartID = partID;
-            PartName = name;
-            Inventory = inventory;
-            Price = price.ToString();
-            Max = max;
-            Min = min;
-        }
-
-        public InHouse(int partID, string name, int inventory, decimal price, int max, int min, int machID)
-        {
-            PartID = partID;
-            PartName = name;
-            Inventory = inventory;
-            Price = price.ToString();
-            Max = max;
-            Min = min;
-            MachineID = machID;
-        }
     }
 }
--- rogers_derek_c968/Models/OutSourced.cs
+++ rogers_derek_c968/Models/OutSourced.cs
@@ -7,29 +7,8 @@ using System.Xml.Linq;
 
 namespace rogers_derek_c968.Models
 {
-    public class OutSourced : Part
+    public class Outsourced : Part
     {
-        private string compName;
-        public string CompName { get; set; }
-        public OutSourced() { }
-        public OutSourced(int partID, string name, int inventory, decimal price, int max, int min)
-        {
-            PartID = partID;
-            PartName = name;
-            Inventory = inventory;
-            Price = price.ToString();
-            Max = max;
-            Min = min;
-        }
-        public OutSourced(int partID, string name, int inventory, decimal price, int max, int min, string compName)
-        {
-            PartID = partID;
-            PartName = name;
-            Inventory = inventory;
-            Price = price.ToString();
-            Max = max;
-            Min = min;
-            CompName = compName;
-        }
+        public string CompanyName { get; set; }
     }
 }
