3bfb92e71d37fdd506a436edf98d21a054c04eba
revrodge <derekrogers007@gmail.com>
2025-06-13 19:53:10 -0600

    Add project files.

---
+++ rogers_derek_c968/Forms/AddPartForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rogers_derek_c968.Forms
+{
+    public partial class AddPartForm : Form
+    {
+        public AddPartForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ rogers_derek_c968/Forms/MainForm.cs
@@ -0,0 +1,10 @@
+namespace rogers_derek_c968
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
